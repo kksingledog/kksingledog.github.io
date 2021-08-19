b6655fb0551167aca0eb4551738c25cb4ac9aae4
Shepherd <lovein3109@gmail.com>
2021-08-19 16:53:35 +0800

    blog

---
+++ accordion.js
@@ -0,0 +1,12 @@
+$("#btn1").click(function(){                                 //click事件
+    $("#photochange").attr("src","produce.png");      //要更換的圖片位置
+});
+$("#btn2").click(function(){                                 //click事件
+    $("#photochange").attr("src","produce2.jpg");      //要更換的圖片位置
+});
+$("#btn3").click(function(){                                 //click事件
+    $("#photochange").attr("src","produce.png");      //要更換的圖片位置
+});
+$("#btn4").click(function(){                                 //click事件
+    $("#photochange").attr("src","produce.png");      //要更換的圖片位置
+});
